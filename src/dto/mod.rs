2616a60532f82807905d0679dc83722ef9d88d2c
Mario Monteiro <marioammonteiro@gmail.com>
2021-06-01 16:55:42 +0100

    try fix pub async fn ws_index(

---
+++ src/dto/mod.rs
@@ -0,0 +1,7 @@
+mod post_input;
+mod post_response;
+// mod web_socket_message;
+
+pub use post_input::PostInput;
+pub use post_response::PostResponse;
+// pub use web_socket_message::WebSocketMessage;
---
+++ src/dto/web_socket_message.rs
@@ -0,0 +1,6 @@
+// use serde::{Serialize};
+
+// #[derive(Serialize, Debug)]
+// pub struct WebSocketMessage {
+//   fingerprint: String,
+// }
